8211cf7d5164111ff327e48853b17ebb5c41222f
nagasrisreya <punnavajhalanagasrisreya@gmail.com>
2025-02-26 16:02:19 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Text, HStack, Button, Input, useColorMode } from '@chakra-ui/react';
+import { Container, Flex, Text, HStack, Button, Input, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { PlusSquareIcon, CloseIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 import { IoMoon } from 'react-icons/io5';
@@ -21,13 +21,36 @@ const Navbar = ({ setSearchTerm }) => {
   };
 
   return (
-    <Container maxW={"1140px"} px={4}>
-      <Flex h={16} alignItems={"center"} justifyContent={"space-between"} flexDir={{ base: "column", sm: "row" }}>
-        <Text fontSize={{ base: "22", sm: "30" }} fontWeight="bold" textTransform="uppercase"
-          textAlign="left" bgGradient="linear(to-r, cyan.400, blue.500)" bgClip="text">
+<Container 
+      maxW="1250px" 
+      px={4} 
+      py={5} 
+      position="sticky" 
+      top={10} 
+      zIndex={10} 
+      bg={useColorModeValue("white", "gray.800")} 
+      shadow="md"
+    >
+      <Flex 
+        h={16} 
+        alignItems="center" 
+        justifyContent="space-between" 
+        flexWrap="wrap"
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text 
+          fontSize={{ base: "50px", sm: "35px" }} 
+          fontWeight="bold" 
+          textTransform="uppercase"
+          textAlign="left" 
+          bgGradient="linear(to-r, gray.700,gray.400)" 
+          bgClip="text"
+          ml={{ base: 0, sm: 10 }}
+        >
           <Link to="/"><h1>TRAVEL LOG</h1></Link>
         </Text>
-        <HStack spacing={4}>
+        
+        <HStack spacing={4} mt={{ base: 2, sm: 0 }}>
           <Input
             placeholder="Search by name..."
             value={search}
