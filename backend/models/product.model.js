5f559af2cc4bf7f3a4955edfc1b7734a57ee38df
nagasrisreya <punnavajhalanagasrisreya@gmail.com>
2025-03-03 10:17:31 +0530

    Update product.model.js

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    image: { type: String, required: true }
+    image: { type: String, required: true },
+    state: { type: String, required: true }
+    
 });
 
 const Product = mongoose.model('Product', productSchema);
