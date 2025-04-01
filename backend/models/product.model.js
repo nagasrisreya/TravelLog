e274f3d5597262dc39b4875a42d596171eeadd24
nagasrisreya <punnavajhalanagasrisreya@gmail.com>
2025-04-01 18:44:58 +0530

    Update product.model.js

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    state: { type: String, required: true }
-    
+    state: { type: String, required: true }, // Add state field
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Add user field
+
 });
 
-const Product = mongoose.model('Product', productSchema);
-
-export default Product;
+export default mongoose.model("Product", productSchema);
